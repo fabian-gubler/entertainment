81284774b0b7596dc0d1be36d706da85477ba4ca
Fabian Gubler <fabian.gubler@hotmail.com>
2023-06-14 13:21:36 +0200

    add artifacts

---
+++ src/test/java/com/personal/entertainment/EntertainmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.entertainment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntertainmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
